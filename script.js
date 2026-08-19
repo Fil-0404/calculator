0cfb236e1360428b13c2e3681076471dd2d6c23b
FilDev <filgeo58@gmail.com>
2026-08-19 17:17:33 +0300

    Add calculator operations

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 }
+
+function operate(operator,num1,num2){
+    if(operator==="+"){
+       return add(num1,num2);
+    }else if(operator==="-"){
+       return subtract(num1,num2);
+    }else if(operator==="*"){
+        return multiply(num1,num2);
+    }else{
+      return divide(num1,num2);
+    }
+}
+
+console.log(operate("+", 3, 5));
+console.log(operate("-", 10, 4));
+console.log(operate("*", 3, 4));
+console.log(operate("/", 20, 5));
